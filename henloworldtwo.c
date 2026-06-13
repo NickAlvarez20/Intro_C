dda8e402c390b2ab0ee70d2fd379992f3a714817
NickAlvarez20 <NicholasAAlvarez20@gmail.com>
2026-06-13 01:55:46 -0700

    git commit -m "(update): Add dual IDE build configs and symbolic temp table constants"

--- henloworldtwo.c
+++ henloworldtwo.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
-/* print Fahrenheit-Celsius table
-for fhar = 0,
-20, ..., 300;
-*/
+#define LOWER 0 /* lower limit of temp table*/
+#define UPPER 300 /* upper limit of temp table */
+#define STEP 20 /* step size */
 
 main() {
   int fahr;
-  for (fahr = 300; fahr > 0; fahr = fahr - 20) {
+  for (fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP) {
     printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
   }
   return 0;

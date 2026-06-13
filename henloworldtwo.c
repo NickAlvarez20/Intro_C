100c9b193531430b06fc5257a74e4fa406eed4a9
NickAlvarez20 <NicholasAAlvarez20@gmail.com>
2026-06-13 00:52:58 -0700

    (exercise 1.5): Print in reverse order

--- henloworldtwo.c
+++ henloworldtwo.c
@@ -6,19 +6,9 @@ for fhar = 0,
 */
 
 main() {
-  float fahr, celsius;
-  int lower, upper, step;
-  lower = 0;
-  upper = 300;
-  step = 20;
-  fahr = lower;
-  while (fahr <= upper) {
-    celsius = 5.0 * (fahr - 32.0) / 9.0;
-    printf("%3.0f\t%6.1f\n", fahr, celsius);
-    fahr = fahr + step;
-
-    /* print Fahrenheit-Celsius table
-    for fahr = 0,
-    20, ..., 300;*/
+  int fahr;
+  for (fahr = 300; fahr > 0; fahr = fahr - 20) {
+    printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
   }
+  return 0;
 }

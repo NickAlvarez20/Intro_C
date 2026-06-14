4b713a70256f16d3e523e0bf072d5e589d2f7e4c
NickAlvarez20 <NicholasAAlvarez20@gmail.com>
2026-06-14 15:04:47 -0700

    Practice counting new lines with c, nl, and while getchar() != EOF

--- henloworldtwo.c
+++ henloworldtwo.c
@@ -1,32 +1,15 @@
 #include <stdio.h>
 
-/* count characters in input; 1st version */
+/* count line in input*/
 
 int main(void){
-  double nc;
-
-  for (nc = 0; getchar() != EOF; ++nc)
-    ; // null statement
-
-  printf("%.0f\n", nc);
+  int c, nl;
+  nl = 0;
+  while((c=getchar()) != EOF){
+    if(c == '\n')
+      ++nl;
+    printf("%d\n", nl);
+  }
   return 0;
 }
 
-
-/* create a program that creates an array, allocates memory for it, and fills it with the numbers 1 to 100, and then frees the memory*/
-
-// int main(void){
-//   int *array = malloc(100 * sizeof(int));
-//   if (array == NULL){
-//     printf("Error: Failed to allocate memory\n");
-//     return 1;
-//   }
-//   for (int i = 0; i < 100; i++){
-//     array[i] = i + 1;
-//   }
-    
-  /* use the array here if you want, e.g. printf("%d\n", array[0]); */
-
-//   free(array);
-//   return 0;
-// }
